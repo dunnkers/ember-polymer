fea87c0fb426ca3c8c8fb48b9a780aa630d93630
Dunnkers <dunnkers@hotmail.com>
2016-08-01 16:43:11 +0200

    Add an acceptance test to test custom elements 🤗
    Tests whether we can render a custom element correctly with- or without
    polypill. In this case it is a paper-element.

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,32 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | Polymer | index');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(this.currentURL(), '/');
+  });
+
+  andThen(function() {
+    let done = assert.async();
+
+    let testElement = () => {
+      assert.ok(document.querySelector('paper-button').shadowRoot,
+        'paper-button has shadowRoot');
+      assert.equal(this.$('paper-button').attr('role'), 'button',
+        'paper-button rendered successfully');
+
+      done();
+    };
+
+    if (window.Polymer.Settings.nativeShadow) {
+      testElement();
+    } else {
+      // wait for elements to be polyfilled
+      window.addEventListener('WebComponentsReady', testElement);
+    }
+  });
+});
