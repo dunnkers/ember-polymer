3db70904e7e86c530bd9e48ad9333207aea38034
Dunnkers <dunnkers@hotmail.com>
2016-10-18 22:36:28 +0200

    Automatic polymer element import. ✨
    Based on installed bower or npm packages! 😃

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
 /* jshint node: true */
 'use strict';
 let fs = require('fs');
+let readFile = fs.readFileSync;
+let fileExists = fs.existsSync;
+let writeFile = fs.writeFileSync;
 let path = require('path');
 let MergeTrees = require('broccoli-merge-trees');
 let Vulcanize = require('broccoli-vulcanize');
+var quickTemp = require('quick-temp');
 
 const defaultVulcanizeOptions = {
   inlineCss: true,
   inlineScripts: true
 };
 
+class Package {
+  constructor(dir, name) {
+    this.dir = dir;
+    this.name = name;
+  }
+
+  get path() {
+    return path.join(this.dir, this.name);
+  }
+
+  get link() {
+    let allImportsPath = path.join(this.path, 'all-imports.html');
+    let elementPath = path.join(this.path, `${this.name}.html`);
+    let linkPath = fileExists(allImportsPath) ? allImportsPath : elementPath;
+    linkPath = path.join('..', '..', linkPath);
+
+    return `<link rel="import" href="${linkPath}">`;
+  }
+}
+
+function getPackages(dependencies, dir, packageFile) {
+  return Object.keys(dependencies).map((name) => {
+    if (isWebComponent(dir, name, packageFile)) {
+      return new Package(dir, name);
+    } else {
+      return false;
+    }
+  }).filter((pkg) => !!pkg);
+}
+
+function hasWebComponentsKeyword(path) {
+  if (!fileExists(path)) {
+    return false;
+  }
+
+  let json = JSON.parse(readFile(path));
+  return json && json.keywords && json.keywords.includes('web-components');
+}
+
+function isWebComponent(dir, name, packageFile) {
+  let elementPath = path.join(dir, name, `${name}.html`);
+
+  return fileExists(elementPath) && hasWebComponentsKeyword(path.join(
+    dir, name, packageFile
+  ));
+}
+
 module.exports = {
   name: 'ember-polymer',
+
+  init: function() {
+    if (this._super.init) {
+      this._super.init.apply(this, arguments);
+    }
+
+    let bowerPackages = getPackages(this.project.bowerDependencies(),
+      this.project.bowerDirectory, 'bower.json');
+    let npmPackages = getPackages(this.project.dependencies(),
+      path.basename(this.project.nodeModulesPath), 'package.json');
+    this.elementsToImport = bowerPackages.concat(npmPackages);
+  },
 
   included: function(appOrAddon) {
     this._super.included.apply(this, arguments);
@@ -26,6 +89,23 @@ module.exports = {
     this.vulcanizeOptions = Object.assign(defaultVulcanizeOptions,
       addonOptions.vulcanizeOptions);
     this.projectTree = app.trees.app;
+    this.autoElementImport = addonOptions.autoElementImport || true;
+    this.excludeElements = addonOptions.excludeElements || [];
+
+    if (this.autoElementImport) {
+      this.elementsToImport = this.elementsToImport.filter(
+        (pkg) => !!pkg && !this.excludeElements.includes(pkg.name));
+      this.elementsToImport = this.elementsToImport.map((pkg) => pkg.link);
+
+      // create a temporary directory to store html imports in.
+      quickTemp.makeOrRemake(this, 'tmpImportsDir');
+
+      // write element imports to temporary file
+      writeFile(path.join(this.tmpImportsDir, 'html-imports.html'),
+                this.elementsToImport.join('\n'));
+      this.projectTree = this.tmpImportsDir;
+      this.htmlImportsFile = path.join(this.tmpImportsDir, 'html-imports.html');
+    }
 
     app.import(app.bowerDirectory + '/webcomponentsjs/webcomponents.min.js');
   },
@@ -50,11 +130,13 @@ module.exports = {
 
       let filePath = path.join(this.app.project.root,
                                this.htmlImportsFile);
-      if (fs.existsSync(filePath)) {
+
+      if (this.autoElementImport || fileExists(filePath)) {
         // vulcanize html files, starting at specified html imports file
         let options = Object.assign(this.vulcanizeOptions, {
           input: path.basename(this.htmlImportsFile),
-          output: this.vulcanizeOutput
+          output: this.vulcanizeOutput,
+          stripExcludes: ['.*paper-styles\.html.*']
         });
 
         let vulcanized = new Vulcanize(this.projectTree, options);
