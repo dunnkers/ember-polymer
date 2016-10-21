22c971ba28bf854bbda2cc461dbad0272042d533
Dunnkers <dunnkers@hotmail.com>
2016-10-21 02:19:49 +0200

    Refactor all auto-import code. 🐝

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 /* jshint node: true */
 'use strict';
 let fs = require('fs');
-let fileExists = fs.existsSync;
 let path = require('path');
+let fileExists = fs.existsSync;
+let writeFile = fs.writeFileSync;
 let MergeTrees = require('broccoli-merge-trees');
 let Vulcanize = require('broccoli-vulcanize');
+let quickTemp = require('quick-temp');
 let AutoImporter = require('./lib/auto-importer');
 let Config = require('./lib/config');
 
 module.exports = {
   name: 'ember-polymer',
 
-  included: function(appOrAddon) {
+  included(appOrAddon) {
     this._super.included.apply(this, arguments);
 
     // config
@@ -19,15 +21,25 @@ module.exports = {
     this.options = new Config(this.project, app);
 
     // auto-import elements
-    let autoImporter = new AutoImporter(this.project, this.options, this.ui);
-    autoImporter.autoImport();
+    if (this.options.autoElementImport) {
+      // create a temporary directory to store html imports in.
+      quickTemp.makeOrRemake(this, 'tmpImportsDir', this.name);
+
+      // import elements
+      let importer = new AutoImporter(this.project, this.options, this.ui);
+      let autoImported = importer.importElements();
+      // write to temporary file
+      this.options.htmlImportsFile = path.join(this.tmpImportsDir,
+        path.basename(this.options.htmlImportsFile));
+      writeFile(this.options.htmlImportsFile, autoImported);
+    }
 
     // import webcomponentsjs polyfill library
     app.import(`${app.bowerDirectory}/webcomponentsjs/webcomponents.min.js`);
   },
 
   // insert polymer and vulcanized elements
-  contentFor: function(type) {
+  contentFor(type) {
     if (type === 'head') {
       return `<script>
                 window.Polymer = window.Polymer || {};
@@ -37,15 +49,9 @@ module.exports = {
     }
   },
 
-  postprocessTree: function(type, tree) {
+  postprocessTree(type, tree) {
     if (type !== 'all') {
       return tree;
-    }
-
-    // specified vulcanize output file must be of html extension
-    if (path.extname(this.options.vulcanizeOutput) !== '.html') {
-      throw new Error('[ember-polymer] The `vulcanizeOutput` file ' +
-        `is not a .html file. You specified '${this.options.vulcanizeOutput}'`);
     }
 
     // a html imports file must exist
@@ -55,17 +61,18 @@ module.exports = {
       return tree;
     }
 
-    // vulcanize html files, starting at specified html imports file
-    let options = Object.assign(this.options.vulcanizeOptions, {
-      input: path.basename(this.options.htmlImportsFile),
-      output: this.options.vulcanizeOutput
-    });
-
     // merge normal tree and our vulcanize tree
-    let vulcanize = new Vulcanize(this.options.projectTree, options);
+    let vulcanize = new Vulcanize(path.dirname(this.options.htmlImportsFile),
+                                  this.options.vulcanizeOptions);
     return new MergeTrees([ tree, vulcanize ], {
       overwrite: true,
       annotation: 'Merge (ember-polymer merge vulcanize with addon tree)'
     });
+  },
+
+  postBuild() {
+    if (this.options.autoElementImport) {
+      quickTemp.remove(this, 'tmpImportsDir');
+    }
   }
 };
