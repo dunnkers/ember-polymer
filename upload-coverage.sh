d3c62de9d9b9738287952d6e104a74974659cbcd
Dunnkers <dunnkers@hotmail.com>
2016-10-21 19:20:39 +0200

    Include node-tests and coverage upload in build task. 👀

--- upload-coverage.sh
+++ upload-coverage.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec test/**/*.js && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
