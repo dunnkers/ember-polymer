f46ce379b9068199a3d5f0491f17545c9c0fbc88
Dunnkers <dunnkers@hotmail.com>
2016-10-22 21:15:12 +0200

    Have Travis only produce coverage once per build. 🐝

---
+++ run-travis.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ $COVERAGE ]; then
+  istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec test/**/*.js &&
+    cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js &&
+    rm -rf ./coverage
+else
+  ember try:one $EMBER_TRY_SCENARIO test --skip-cleanup
+fi
--- upload-coverage.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec test/**/*.js && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
