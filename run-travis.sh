3873c54249ea8504bcbd9a99f16748bd6e0cc49b
Dunnkers <dunnkers@hotmail.com>
2016-10-26 22:36:50 +0200

    Add Code Climate test coverage. 🙃

--- run-travis.sh
+++ run-travis.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 if [ $COVERAGE ]; then
-  istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec test/**/*-test.js &&
-    cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js &&
-    rm -rf ./coverage
+  istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec test/**/*-test.js
+  ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
+  ./node_modules/codeclimate-test-reporter/bin/codeclimate.js < ./coverage/lcov.info
+  rm -rf ./coverage
 else
   ember try:one $EMBER_TRY_SCENARIO test --skip-cleanup
 fi
