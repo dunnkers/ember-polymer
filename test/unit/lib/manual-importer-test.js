c8955695bd8f363018690511fbb1c107e2c01c87
Dunnkers <dunnkers@hotmail.com>
2016-12-29 05:43:50 +0100

    Add a test to confirm styles are imported. 😺 Closes #25.
    Only just adding a test actually confirms it was working already all
    along. So I actually unnecessarily opened this issue, ooops. 🤡

--- test/unit/lib/manual-importer-test.js
+++ test/unit/lib/manual-importer-test.js
@@ -1,16 +1,29 @@
 /* jshint node: true */
 'use strict';
 
-var assert = require('assert');
+let assert = require('assert');
+// libs
+let Config = require('../../../lib/config');
+let Packages = require('../../../lib/packages');
+// fixtures
+let app = require('../../fixtures/app-fixture');
+let project = require('../../fixtures/project-fixture');
+let ui = require('../../fixtures/ui-fixture.js');
 
 describe('manual-importer', function() {
-  var ManualImporter;
+  let ManualImporter;
 
   before(function() {
     ManualImporter = require('../../../lib/manual-importer');
+    app.project.root = process.cwd();
+    this.options = new Config(app);
   });
 
   it('loads it', function() {
+    this.packages = new Packages(project);
+    let packages = this.packages.getPackages();
+
+    this.manualImporter = new ManualImporter(this.options, ui, packages);
     assert.ok(ManualImporter);
   });
 
@@ -32,5 +45,12 @@ describe('manual-importer', function() {
 
   it.skip('completely removes imports instead of replacing the line', function() {
 
+  });
+
+  it('also imports all other html inside the htmlImportsFile', function() {
+      this.options.htmlImportsFile = 'test/fixtures/style-element.html';
+      let elements = this.manualImporter.getImports();
+      assert.equal(elements, `<style is="custom-style" include="iron-flex"></style>
+`);
   });
 });
