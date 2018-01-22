1c8eda06eb0680e29dad8c2f76310fad7c51e38e
Dunnkers <jeroenoverschie@gmail.com>
2018-01-22 22:41:08 +0100

    Use `--no-sandbox` on TravisCI 🔨
    Related to use of Headless Chrome, see https://github.com/ember-cli/ember-cli/pull/7566.

--- testem.js
+++ testem.js
@@ -12,6 +12,9 @@ module.exports = {
     Chrome: {
       mode: 'ci',
       args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
