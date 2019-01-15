ab31275548486c6f04b17ab07a801cd8d3cd96ea
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2019-01-15 14:43:48 +0100

    chore(ember): update to latest

--- testem.js
+++ testem.js
@@ -10,16 +10,38 @@ module.exports = {
   ],
   browser_args: {
     Chrome: {
+<<<<<<< HEAD
       mode: 'ci',
       args: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.TRAVIS ? '--no-sandbox' : null,
 
         '--disable-gpu',
+||||||| parent of 3b112e4... message
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+=======
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
+>>>>>>> 3b112e4... message
         '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
+<<<<<<< HEAD
       ]
     },
+||||||| parent of 3b112e4... message
+      ]
+    }
+=======
+      ].filter(Boolean)
+    }
+>>>>>>> 3b112e4... message
   }
 };
